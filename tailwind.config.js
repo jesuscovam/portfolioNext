c5d71011b79bc284bfe536ac62415d60ebbd95bb
jesuscovam <jesuscovam@icloud.com>
2020-10-04 19:17:23 -0500

    como repositorio de next con tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
 	future: {
-		// removeDeprecatedGapUtilities: true,
-		// purgeLayersByDefault: true,
+		removeDeprecatedGapUtilities: true,
 	},
-	purge: [],
+	purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'accent-1': '#333',
+			},
+		},
 	},
 	variants: {},
 	plugins: [],
